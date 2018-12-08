2fc198cfffbc302e37f8f7d22ee55c6bce6f3833
gvelrajan <36194455+gvelrajan@users.noreply.github.com>
2018-12-08 20:15:51 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -4,7 +4,7 @@ var rand = Math.floor(Math.random() * 100);
 //create a server object:
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('Hello World!'); //write a response to the client
+res.write('Hello John!'); //write a response to the client
 res.write('My number is: ' + rand); //write a response to the client
 res.end(); //end the response
 }).listen(80); //the server object listens on port 80
